494a64f6ae783301ae8ebcf20a9af4775274b485
Aren Elchinyn <slizen.can@gmail.com>
2022-04-17 21:49:52 +0300

    Update vesa_Russia.c

--- examples/C/vesa_Russia.c
+++ examples/C/vesa_Russia.c
@@ -1,20 +1,24 @@
 #include <vesa.h>
 
 
+#define x_start 768
+#define x_end 1024
+
+
 int main(){
-    for (int x = 256; x != 512; x++){
+    for (int x = x_start; x != x_end; x++){
         for (int y = 256; y != 320; y++){
             draw_pixel(x, y, VESA_WHITE);
         }
     }
 
-    for (int x = 256; x != 512; x++){
+    for (int x = x_start; x != x_end; x++){
         for (int y = 320; y != 384; y++){
             draw_pixel(x, y, VESA_BLUE);
         }
     }
 
-    for (int x = 256; x != 512; x++){
+    for (int x = x_start; x != x_end; x++){
         for (int y = 384; y != 448; y++){
             draw_pixel(x, y, VESA_RED);
         }
