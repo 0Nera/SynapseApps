de96addeed9a3121c6ef642a444a59d9f6a1a3f4
Aren Elchinyn <slizen.can@gmail.com>
2022-04-17 21:49:23 +0300

    Update build.py

--- build.py
+++ build.py
@@ -15,15 +15,17 @@ def build_all():
         print(E)
     
     print("Building apps")
-    os.system("i686-elf-gcc -nostdlib -lgcc -ffreestanding -I include/ -c examples/C/HelloWorld.c -o ./bin/HelloWorld.o")
-    os.system("i686-elf-gcc -nostdlib -lgcc -ffreestanding -I include/ -c examples/C/sound.c -o ./bin/beep.o")
-    os.system("i686-elf-gcc -nostdlib -lgcc -ffreestanding -I include/ -c examples/C/popsort_int_test.c -o ./bin/popsort_int_test.o")
-    os.system("i686-elf-gcc -nostdlib -lgcc -ffreestanding -I include/ -c examples/C/vesa_Russia.c -o ./bin/vesa_Russia.o")
+    os.system("i686-elf-gcc -nostdlib -w -lgcc -ffreestanding -I include/ -c examples/C/HelloWorld.c -o ./bin/HelloWorld.o")
+    os.system("i686-elf-gcc -nostdlib -w -lgcc -ffreestanding -I include/ -c examples/C/sound.c -o ./bin/beep.o")
+    os.system("i686-elf-gcc -nostdlib -w -lgcc -ffreestanding -I include/ -c examples/C/popsort_int_test.c -o ./bin/popsort_int_test.o")
+    os.system("i686-elf-gcc -nostdlib -w -lgcc -ffreestanding -I include/ -c examples/C/vesa_Russia.c -o ./bin/vesa_Russia.o")
+    os.system("i686-elf-gcc -nostdlib -w -lgcc -ffreestanding -I include/ -c examples/C/char.c -o ./bin/char.o")
     
     os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/hi.elf ./bin/HelloWorld.o")
-    os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/sort_int.elf ./bin/popsort_int_test.o")
+    os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/sort.elf ./bin/popsort_int_test.o")
     os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/Russia.elf ./bin/vesa_Russia.o")
     os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/beep.elf ./bin/beep.o")
+    os.system("i686-elf-gcc -nostdlib -lgcc -T link.ld -o ../bin/apps/char.elf ./bin/char.o")
 
 
 if __name__ == "__main__":
