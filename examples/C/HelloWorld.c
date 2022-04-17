7e2e4d8dbb970d51545f797ab137add7cc8b7c5a
Aren Elchinyn <slizen.can@gmail.com>
2022-04-17 20:36:32 +0300

    Update HelloWorld.c

--- examples/C/HelloWorld.c
+++ examples/C/HelloWorld.c
@@ -5,14 +5,5 @@
 int main(){
     print_str("Hello World!");
 
-    /*
-    while(1){
-        char c[] = {'\0', '\0'};
-        c[1] = getchar() + 16;
-        qemu_putstring("APP:[");
-        qemu_putstring(c[0]);
-        qemu_putstring("]");
-        print_str(c[0]);
-    }*/
     return 0;
 }
