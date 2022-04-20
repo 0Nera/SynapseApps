a5aa33f3135744d86496aed5863ea8e029c472fb
Aren Elchinyn <slizen.can@gmail.com>
2022-04-20 08:19:34 +0300

    Fix popsort algoritm

--- examples/C/popsort_int_test.c
+++ examples/C/popsort_int_test.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-#include <learntask.h>
 
 
 int main(){
-    int test[] = { 32, 4, 8, 0};
+    int test[] = {32, 4, 8, 0};
+    int n = sizeof(test) / sizeof(int);
     
-    popsort_int(test);
-    
-
-    for (int i = 0; i != 4; i++){
-        printf("%d: %d", i, test[i]);
+    for(int i = 0 ; i < n - 1; i++) { 
+       // сравниваем два соседних элемента.
+       for(int j = 0 ; j < n - i - 1 ; j++) {  
+           if(test[j] > test[j+1]) {           
+              // если они идут в неправильном порядке, то  
+              //  меняем их местами. 
+              int tmp = test[j];
+              test[j] = test[j+1] ;
+              test[j+1] = tmp; 
+           }
+        }
     }
 
+    for (int i = 0; i != sizeof(test) / sizeof(int); i++){
+        printf("\n%d: %d", i, test[i]);
+    }
     return 0;
 }
--- include/learntask.h
+++ include/learntask.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 int fibonacci(int N) {
     if (N == 1 || N == 2)
         return 1; // первые 2 числа равны 1
@@ -8,6 +10,12 @@ int fibonacci(int N) {
 void popsort_int(int a[]) {
     // считываем количество чисел n
     int n = sizeof(a) / sizeof(int);
+    int temp[n];
+
+    for(int i = 0; i != n; i++){
+        temp[i] = a[i];
+        printf("\n%d: %d", i, temp[i]);
+    }
 
     for(int i = 0 ; i < n - 1; i++) { 
        // сравниваем два соседних элемента.
@@ -15,10 +23,14 @@ void popsort_int(int a[]) {
            if(a[j] > a[j+1]) {           
               // если они идут в неправильном порядке, то  
               //  меняем их местами. 
-              int tmp = a[j];
-              a[j] = a[j+1] ;
-              a[j+1] = tmp; 
+              int tmp = temp[j];
+              temp[j] = temp[j+1] ;
+              temp[j+1] = tmp; 
            }
         }
+    }
+
+    for (int i = 0; i != sizeof(temp) / sizeof(int); i++){
+        printf("\n%d: %d", i, temp[i]);
     }
  }
