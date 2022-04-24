0cb0c5335a4cd4c5da58519798a46561123fa919
Aren Elchinyn <slizen.can@gmail.com>
2022-04-24 16:30:40 +0300

    Update messages.h

--- apps/0Nera/NeraMath/messages.h
+++ apps/0Nera/NeraMath/messages.h
@@ -2,4 +2,4 @@
 #define FILE_READ_ERROR "ERROR: File read error"
 #define INVALID_CHARACTER "ERROR: Invalid character"
 #define help "\nHelp:\n\n    ? - show info and help\n    e - exit\n\n    Sample:\n$NeraMath '1+1'\n\n    You can use NeraMath no parameters:\n$NeraMath\n>1+1\nResult: 2\n\n"
-#define info "\nNeraMath operator.\n    NeraMath it is a primitive mathematical interpreter\nCreated by Aren Elchinyan in 2021\nGithub: github.com/math_interpreter\n\n"
+#define info "\nNeraMath operator.\n    NeraMath it is a primitive mathematical interpreter\nCreated by Aren Elchinyan in 2021\nGithub: https://github.com/0Nera/NeraMath\n\n"
